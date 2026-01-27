6a4b0b7bcb561dd8e0fa358c34cb49011a334dfd
Margyyy7 <kalolamargi7@gmail.com>
2026-01-27 09:10:08 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,48 @@
+#x = "hello"
+#print(x)
+#A = input("Enter value :")
+#B = input("Enter value : ")
+#total = A+B
+#print(total)
+#num = int(input( "Enter a number: ")
+#if num%2==0：
+# print(f"{num} is Even")
+#else:
+#print(f" {num} is Odd")
+#year = input("Enter year: ")
+#if(year % 4 ==0)
+#print( "Leap year")
+#else:
+#print("not a Leap year")
+#import numpy as np 
+#print(np.pi)
+base = float(input("Enter the base: "))
+#square = number**2
+#print (square)
+#import math
+#radius = float (input("Enter value:"))
+#area = math.pi * (radius**2)
+#print(f"Area of circle: {area: 2f}"
+#user_input - input ("Enter: ")
+#try:
+   #val - int(user_input)
+   #print("This is integer")
+   #except ValueError:
+   #try:
+      #val - float(user_input)
+	  #print("This is float")
+	 #except ValueError:
+      #print("This is string")
+#base = float(input("Enter"))
+base = float(input("Enter the base: "))
+exponent = float(input("Enter the exponent: "))
+result = pow(base, exponent)
+print(f"Result: {result}")
+#num = int(input( "Enter a number: ")
+if number > 0:
+        print("The number is positive")
+elif number < 0:
+        print("The number is negative")
+else:
+        print("The number is zero") 
+		
