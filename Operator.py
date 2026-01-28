1b0dc51d9056c444192336d394a4916e64ff239e
Margyyy7 <kalolamargi7@gmail.com>
2026-01-28 23:43:17 +0530

    Add files via upload

---
+++ Operator.py
@@ -0,0 +1,40 @@
+#arithmetic
+#a = 5
+#b = 10
+#print(a+b)
+#print(a-b)
+#print(a/b)
+#assignment
+#x= 10
+#print(x)
+#unary minus
+#a= 5
+#print(-a)
+#relational
+#p= 10
+#q= 5
+#print(p>q)
+#print(q<p)
+#logical
+#x= True
+#y= False
+#print(x and y)
+#print(x or y)
+#print(not x)
+#boolean
+#is_pass= True
+#print(is_pass)
+#bitwise
+#a= 5
+#b= 3
+#print(a and b)
+#print(a/b)
+#membership
+#numbers= [1,2,3,4]
+#print(2 in numbers)
+#print(5 not in numbers)
+#identify
+#x= 10
+#y= 10
+#print(x is y)
+#print(x is not y)
