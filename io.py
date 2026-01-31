004ef08d21966edc5f4b67165fa26b2ca73264f4
Margyyy7 <kalolamargi7@gmail.com>
2026-01-31 08:13:39 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,14 @@
+#Name = input("Enter name : ")
+#print("Hello", Name)
+
+#Hobby = input("Enter your hobby : ")
+#print("My hobby is",Hobby)
+
+#Food = input("Enter fav dishes: ")
+#print("I enjoy",Food)
+
+#print("Here we go!")
+
+#print("jan and dec are the only month which is so close yet so far")
+
+print("Hello world")
