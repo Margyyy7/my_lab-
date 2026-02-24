fbc3a61ffae48a3b557757339ae00459f52dbbb2
Margyyy7 <kalolamargi7@gmail.com>
2026-02-24 08:20:41 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,57 @@
+#print("Num from 1 to 10")
+#i=1
+#while i<10:
+ #   print(i)
+#    i=i+1
+#print("Sum of natural num")
+#n=int(input("Enter n : "))
+#i=1
+#s=0
+#while i<=n:
+  #  s=s+i
+ #   i=i+1
+#print("Sum =",s)    
+#print("Table of a number")
+#num=int(input("Enter number : "))
+#i=1
+#while i<=10:
+#    print(num,"x",i,"=",num*i)
+#    i=i+1
+#print("Num from 1 to 5")
+#i=1
+#while i<=5:
+    #print(i)
+   # i=i+1
+#print("Sum of number")
+#n=int(input("Enter number : "))
+#i=1
+#s=0
+#while i<=n:
+  #  s=s+i
+ #   i=i+1
+#print("Sum =",s)
+#print("Print odd num")
+#i=1
+#while i<=20:
+ #   print(i)
+#    i=i+2
+#print("Table of 4")
+#num=int(input("Enter num : "))
+#i=1
+#while i<=10:
+ #   print(num,"x",i,"=",num*i)
+#    i=i+1
+#print("Reverse num")
+#i=20
+#while i>=1:
+   # print(i)
+   # i=i-1
+#print("Even num")
+#i=2
+#while i<=20:
+ #   print(i)
+#    i=i+2
+print("largest_num")
+n=[10,20,30,40,50]
+largest_num = max(n)
+print(largest_num)
