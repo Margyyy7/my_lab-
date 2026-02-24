db30c51e191d7f295b592dc78d5ec521a53200ce
Margyyy7 <kalolamargi7@gmail.com>
2026-02-24 08:20:18 +0530

    Add files via upload

---
+++ for_loop.py
@@ -0,0 +1,29 @@
+#print('Numbers from 1 to 10')
+#for i in range(1,11):
+#    print(i)
+#print('Even Numbers from 1 to 20')
+#for i in range(1,21):
+ #  if i % 2 == 0:
+#     print(i)
+#print('Odd Numbers from 1 to 15')
+#for i in range(1,16):
+  #  if i % 2 != 0:
+ #       print(i)
+#print('Table of 15')
+#for i in range(1,11):
+ #   print("15 x ", i ," = " , 15*i)
+#print('characters of string')
+#name="Atmiya"
+#for letter in name:
+#    print(letter)
+#print('sum of num form 1 to 5')
+#total = 0
+#for i in range(1,6):
+ # total = total+i
+#  print("sum is :",total)
+print('list elements')
+numbers = [10,20,30,40]
+for n in numbers:
+  print(n)
+
+
