1cda96c4f9da4d8e820332c33f721e322c8fa8d3
Margyyy7 <kalolamargi7@gmail.com>
2026-03-03 08:36:14 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,51 @@
+#INTEGER array
+from array import array
+arr=array('i',[10,20,30,40])
+print(arr)
+print(type(arr))
+
+#LENGTH
+#from array import array
+#arr=array('i',[10,20,30,40])
+#print(len(arr))
+
+#APPEND
+#from array import array
+#arr=array('i',[10,20,30])
+#arr.append(40)
+#print(arr)
+
+#INSERT
+#from array import array
+#arr=array('i',[10,20,40])
+#arr.insert(2,30)
+#print(arr)
+
+#REMOVE
+#from array import array
+#arr=array('i',[10,20,30,20,40])
+#arr.remove(20)
+#print(arr)
+
+#POP
+#from array import array
+#arr=array('i',[10,20,30,40])
+#x=arr.pop()
+#print('Removed:',x)
+#print(arr)
+
+#INDEX
+#from array import array
+#arr=array('i',[10,20,30,40])
+#print(arr.index(30))
+
+#COUNT
+#from array import array
+#arr=array('i',[10,20,30,20,40])
+#print(arr.count(20))
+
+#REVERSE
+#from array import array
+#arr=array('i',[10,20,30,40])
+#arr.reverse()
+#print(arr)
