50509f1d205064848252c8aaa51aacd536da9a82
Margyyy7 <kalolamargi7@gmail.com>
2026-03-07 09:05:23 +0530

    Add files via upload

--- array.py
+++ array.py
@@ -1,51 +1,47 @@
-#INTEGER array
-from array import array
-arr=array('i',[10,20,30,40])
-print(arr)
-print(type(arr))
-
-#LENGTH
+#print("Positive")
 #from array import array
 #arr=array('i',[10,20,30,40])
-#print(len(arr))
-
-#APPEND
+#print(arr[0])
+#print(arr[2])
+#print(arr[3])
+#print("Negative")
 #from array import array
-#arr=array('i',[10,20,30])
-#arr.append(40)
+#arr=array('i',[10,20,30,40,50])
+#print(arr[-1])
+#print(arr[-2])
+#print(arr[-5])
+#print("Modify")
+#from array import array
+#arr=array('i',[10,20,30,40,50])
+#arr[2]=35
+#print(arr)
+#print("Array")
+#from array import array
+#arr=array('i',[10,20,30,40,50])
+#print(arr[1:4])
+#print(arr[:3])
+#print(arr[2:])
+#print(arr[:])
+#print("slicing")
+#from array import array
+#arr=array('i',[10,20,30,40,50,60,70,80])
+#print(arr[::2])
+#print(arr[1::2])
+#print(arr[::3])
+#print("Negative")
+#from array import array
+#arr=array('i',[10,20,30,40,50])
+#print(arr[-4:-1])
+#print(arr[-3:])
+#print(arr[:-2])
+#print("Reverse")
+#from array import array
+#arr=array('i',[10,20,30,40,50])
+#print(arr[::-1])
+#print("Modifying")
+#from array import array
+#arr=array('i',[10,20,30,40,50])
+#arr[1:4]=array('i',[25,35,45])
 #print(arr)
 
-#INSERT
-#from array import array
-#arr=array('i',[10,20,40])
-#arr.insert(2,30)
-#print(arr)
 
-#REMOVE
-#from array import array
-#arr=array('i',[10,20,30,20,40])
-#arr.remove(20)
-#print(arr)
-
-#POP
-#from array import array
-#arr=array('i',[10,20,30,40])
-#x=arr.pop()
-#print('Removed:',x)
-#print(arr)
-
-#INDEX
-#from array import array
-#arr=array('i',[10,20,30,40])
-#print(arr.index(30))
-
-#COUNT
-#from array import array
-#arr=array('i',[10,20,30,20,40])
-#print(arr.count(20))
-
-#REVERSE
-#from array import array
-#arr=array('i',[10,20,30,40])
-#arr.reverse()
-#print(arr)
