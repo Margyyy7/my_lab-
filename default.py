c36b2be2767637e355bdd43743f3f14d15a89b3d
Margyyy7 <kalolamargi7@gmail.com>
2026-03-31 08:47:35 +0530

    Add files via upload

---
+++ default.py
@@ -0,0 +1,14 @@
+#def greet(name, msg= "Good Morning"):
+ #   print("Hello",name + ",",msg)
+
+#greet("Asha")
+#def power(num,exp=2):
+ #   return num**exp
+
+#print(power(9))        
+def student_info(name,age=18,course="IT"):
+    print("Name: ",name)
+    print("Age: ",age)
+    print("Course: ",course)
+
+student_info("Ravi")    
