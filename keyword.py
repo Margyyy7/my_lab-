98ca584719b5c8e57112604ff149f4953e81ab7e
Margyyy7 <kalolamargi7@gmail.com>
2026-03-31 08:47:57 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,16 @@
+#def student_info(name,age,city):
+   # print("Name:",name)
+  #  print("Age:",age)
+ #   print("City:",city)
+
+#student_info(age=18, city="Rajkot",name="Ravi")    
+#def display(a,b,c):
+   # print("A =",a)
+  #  print("B =",b)
+ #   print("C =",c)
+#display(1, c=3, b=2)   
+#def simple_interest(p:float,r:int,t:float):
+  #  si=(p*r*t)/100
+ #   print("Simple Interest:",si) 
+
+#simple_interest(p=1000, t=2, r=1.5)    
