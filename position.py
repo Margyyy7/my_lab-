d15518a1f2fb027e36fcdc6e4f644341f3174628
Margyyy7 <kalolamargi7@gmail.com>
2026-03-31 08:48:17 +0530

    Add files via upload

---
+++ position.py
@@ -0,0 +1,39 @@
+#def add(a,b):
+   # print("a=",a)
+  #  print("b=",b)
+ #   print(a+b)
+#result=add(2,5)
+#print("sum=",result)   
+#def student_info(name,roll,marks):
+   # print("Name:",name)
+  #  print("Roll No:",roll)
+ #   print("Marks:",marks)
+#student_info("Ravi",101,85)    
+#def simple_interest (p,r,n):
+    #si=(p*r*n)/100
+   # print("simple interest: ",si)
+  #  simple_interest(1000,2,2)
+#def check_value(no):
+   #if(no>0):
+    #  print("positive")
+   #elif(no<0):
+   #   print("negative")
+  # else:
+  #    print("zero")
+ #  check_value(59) 
+#def odd_even(no):
+   #if(no/2==0):
+   #   print(f,'value {no} is even')
+  # else:
+ #     print(f,'value {no} is odd') 
+#odd_even(30)
+#odd_even(5)     
+#def addition(a,b):
+  # add=a+b
+ #  print("Addition of two values",add)
+#addition(50,95)   
+
+    
+
+    
+
