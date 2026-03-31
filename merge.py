1c11f3122a86fd724af9c41c9a4d4fde1b05a2b3
Margyyy7 <kalolamargi7@gmail.com>
2026-03-31 08:49:50 +0530

    Add files via upload

---
+++ merge.py
@@ -0,0 +1,6 @@
+with open("one.txt","r") as f1, open ("two.txt","r") as f2, open("create.txt","w") as f3:
+    f3.write(f1.read())
+    f3.write("\n")
+    f3.write(f2.read())
+
+print("File merged succesfully")    
